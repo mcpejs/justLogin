cb13f387731c1447db392844ef5cd0a9616e7283
mcpejs <mcpejs1@gmail.com>
2020-03-04 13:12:24 +0900

    로그인 분기및 로그아웃 추가
    프론트엔드단의 로그인 분기와 백엔드에 로그아웃을 추가함

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const path=require('path')
+const crypto=require('crypto')
 const router=express.Router()
 let db=require('../db/db')
 
@@ -16,6 +17,7 @@ router.post('/login',function(req,res){
     
     let bodynickname=req.body.nickname
     let bodypassword=req.body.password
+    let hashedpass=crypto.createHash('sha256').update(bodypassword).digest('hex')
 
     let getpassquery='select password from accounts where name=?'
     db.query(getpassquery,bodynickname,function(err,data){
@@ -35,7 +37,7 @@ router.post('/login',function(req,res){
                 let datapassword=data[0].password
 
                 // 비밀번호가 맞았을떄
-                if(bodypassword==datapassword){
+                if(hashedpass==datapassword){
                     req.session.isLogin=true
                     req.session.nickname=bodynickname
                     res.status(200)
@@ -59,8 +61,10 @@ router.post('/register',function(req,res){
     
     let bodynickname=req.body.nickname
     let bodypassword=req.body.password
-    
-    let account={name:bodynickname,password:bodypassword}
+    let hashedpass=crypto.createHash('sha256').update(bodypassword).digest('hex')
+    console.log(bodypassword+'\n'+hashedpass)
+
+    let account={name:bodynickname,password:hashedpass}
     let createaccountquery='insert into accounts set ?'
     db.query(createaccountquery,account,function(err,data){
         if(err){
@@ -72,6 +76,20 @@ router.post('/register',function(req,res){
             res.send('<script type="text/javascript">alert("계정이 성공적으로 생성되었습니다.");location.href="/"</script>')
         }
     })
+})
+
+// get이지만 보여주는 화면이없어 백엔드로 분류
+router.get('/logout',function(req,res){
+    if(req.session.isLogin==true) {
+        // 만약 로그인됐다면 세션정보 삭제
+        req.session.destroy(function(){
+            res.send('<script type="text/javascript">alert("성공적으로 로그아웃되었습니다");location.href="/"</script>')
+        })
+    }
+    else {
+        // 만약 로그인되지 않았다면 로그인 페이지로 이동
+        res.redirect('/login')
+    }
 })
 
 // 값없는 변수가 포함되있는지 확인하는 함수
--- routes/index.js
+++ routes/index.js
@@ -15,19 +15,34 @@ router.get('/',function(req,res){
 })
 
 router.get('/profile',function(req,res){
-    res.render('../views/myprofile',{nickname:req.session.nickname})
+    if(req.session.isLogin==true) {
+        // 만약 로그인됐다면 프로필 페이지 보여주기
+        res.render('../views/myprofile',{nickname:req.session.nickname})
+    }
+    else {
+        // 만약 로그인되지 않았다면 로그인 페이지로 이동
+        res.redirect('/login')
+    }
 })
 
 router.get('/login',function(req,res){
-    res.sendFile(path.resolve('./views/loginpage.html'))
+    if(req.session.isLogin==true) {
+        // 만약 로그인됐다면 프로필 페이지로 이동
+        res.redirect('/profile')
+    }
+    else {
+        res.sendFile(path.resolve('./views/loginpage.html'))
+    }
 })
 
 router.get('/register',function(req,res){
-    res.sendFile(path.resolve('./views/register.html'))
-})
-
-router.get('/test',function(req,res){
-    res.sendFile(path.resolve('./views/test.html'))
+    if(req.session.isLogin==true) {
+        // 만약 로그인됐다면 프로필 페이지로 이동
+        res.redirect('/profile')
+    }
+    else {
+        res.sendFile(path.resolve('./views/register.html'))
+    }
 })
 
 module.exports=router
